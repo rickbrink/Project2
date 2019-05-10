331d5a213cd9e0423978acaa8e2fe888377f6199
Rick <rickbrink94@gmail.com>
2019-05-10 14:41:21 +0200

    Data Table aangepast

--- data_table.php
+++ data_table.php
@@ -9,7 +9,7 @@
     <title>Template</title>
 
     <!-- Bootstrap CSS CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/datatables/1.10.12/css/dataTables.bootstrap.min.css" />
     <!-- Our Custom CSS -->
     <link rel="stylesheet" href="css/style.css">
@@ -58,7 +58,49 @@
                 </div>
             </nav>
 
+            <div class="container">
+              <?php
+              $mysqli = new mysqli('localhost', 'root', 'root', 'Project2') or die(mysqli_error($mysqli));
+              $result = $mysqli->query("SELECT * FROM gebruiker INNER JOIN spel ON gebruiker.spel_id = spel.id") or die($mysqli->error);
+              //pre_r($result);
+              ?>
 
+              <div class="row justify-content-center">
+                <table class="table table-striped table-bordered table-hover" id="myTable">
+                  <thead>
+                    <tr>
+                      <th>Naam</th>
+                      <th>Adres</th>
+                      <th>Postcode</th>
+                      <th>Woonplaats</th>
+                      <th>Email</th>
+                      <th>Categorie</th>
+                      <th>Soort</th>
+                      <th>Bordspel</th>
+                      <th>Game</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    while ($row = $result->fetch_assoc()): ?>
+                    <tr>
+                      <td><?php echo $row['voornaam']; ?> <?php echo $row['tussenvoegsel']; ?> <?php echo $row['achternaam']; ?></td>
+                      <td><?php echo $row['adres']; ?></td>
+                      <td><?php echo $row['postcode']; ?></td>
+                      <td><?php echo $row['woonplaats']; ?></td>
+                      <td><?php echo $row['email']; ?></td>
+                      <td><?php echo $row['categorie']; ?></td>
+                      <td><?php echo $row['soort']; ?></td>
+                      <td><?php echo $row['bordspel']; ?></td>
+                      <td><?php echo $row['game']; ?></td>
+                    </tr>
+                  <?php endwhile; ?>
+                </tbody>
+
+              </table>
+            </div>
+
+          </div>
 
         </div>
     </div>
